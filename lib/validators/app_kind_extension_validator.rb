1c56437aa2944f01e88fe57f3905a28cfa3c3c34
Alex Naumov <alexsnaumov@gmail.com>
2013-01-22 15:43:47 +0700

    hotfix http://errbit.brandymint.ru/apps/5077f320687d9c7dde000095/problems/50fa4836687d9c4bd40860ef

--- lib/validators/app_kind_extension_validator.rb
+++ lib/validators/app_kind_extension_validator.rb
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 class AppKindExtensionValidator < ActiveModel::EachValidator
   def validate_each (record, attribute, value)
-    file_ext = record.source_file.file.extension.downcase
-    app_file_ext = record.app.kind.ext
-    record.errors[attribute] << "расширение файла .#{file_ext} не совпадает с указаным типом приложения .#{app_file_ext}" if  file_ext != app_file_ext
+    if record.source_file.present?
+      file_ext = record.source_file.file.extension.downcase 
+      app_file_ext = record.app.kind.ext
+      record.errors[attribute] << "расширение файла .#{file_ext} не совпадает с указаным типом приложения .#{app_file_ext}" if  file_ext != app_file_ext
+    end  
   end
 end
